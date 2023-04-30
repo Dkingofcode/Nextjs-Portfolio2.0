0c6c593c9ce1277ad48f3f623b73db5d05adcb2f
Dkingofcode <doladepo128@gmail.com>
2023-04-30 22:30:51 +0100

    imported Header component into index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,18 @@
-import Image from 'next/image'
+import Image from 'next/image';
 //import { Inter } from 'next/font/google'
-
+import Head from "next/head";
 //const inter = Inter({ subsets: ['latin'] })
+import Header from "../components/Header";
+import type { NextPage } from 'next';
 
-export default function Home() {
+
+const Home: NextPage = () => {
   return (
+
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24`}>
-
+      <Header />
     </main>
   )
 }
+
